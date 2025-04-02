2e2e4e424ef16a6534981c34a6a5c8bb251b3370
Kosteyyy <averkiev@yandex.ru>
2025-04-02 21:00:09 +0300

    Первый коммит

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductComponent } from './component';
+
+@NgModule({
+  declarations: [
+    ProductComponent,
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [ProductComponent]
+})
+export class AppModule { }
