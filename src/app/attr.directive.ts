b8707b070831cb6d052a375d587a78175dc40a10
Kosteyyy <7456052@gmail.com>
2025-04-05 00:17:51 +0300

    HostElementBinding

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -3,6 +3,8 @@ import {
   Directive,
   ElementRef,
   EventEmitter,
+  HostBinding,
+  HostListener,
   Input,
   Output,
   SimpleChanges,
@@ -11,16 +13,16 @@ import { Product } from './product.model';
 
 @Directive({ selector: '[pa-attr]' })
 export class PaAttrDirective {
-
-  constructor(private element: ElementRef) {
-    this.element.nativeElement.addEventListener('click', () => {
-      if (this.product != null) {
-        this.click.emit(this.product.category);
-      }
-    });
-  }
+  // constructor(private element: ElementRef) {
+  //   this.element.nativeElement.addEventListener('click', () => {
+  //     if (this.product != null) {
+  //       this.click.emit(this.product.category);
+  //     }
+  //   });
+  // }
 
   @Input('pa-attr')
+  @HostBinding('class')
   bgClass: string | null = '';
 
   @Input('pa-product')
@@ -29,15 +31,22 @@ export class PaAttrDirective {
   @Output('pa-category')
   click = new EventEmitter<string>();
 
-  ngOnChanges(changes: SimpleChanges) {
-    console.log('🚀 ~ PaAttrDirective ~ ngOnChanges ~ changes:', changes);
-    let change = changes['bgClass'];
-    let classList = this.element.nativeElement.classList;
-    if (!change.isFirstChange() && classList.contains(change.previousValue)) {
-      classList.remove(change.previousValue);
-    }
-    if (!classList.contains(change.currentValue)) {
-      classList.add(change.currentValue);
+  // ngOnChanges(changes: SimpleChanges) {
+  //   console.log('🚀 ~ PaAttrDirective ~ ngOnChanges ~ changes:', changes);
+  //   let change = changes['bgClass'];
+  //   let classList = this.element.nativeElement.classList;
+  //   if (!change.isFirstChange() && classList.contains(change.previousValue)) {
+  //     classList.remove(change.previousValue);
+  //   }
+  //   if (!classList.contains(change.currentValue)) {
+  //     classList.add(change.currentValue);
+  //   }
+  // }
+
+  @HostListener('click')
+  triggerCustomEvent() {
+    if (this.product != null) {
+      this.click.emit(this.product.category);
     }
   }
 }
