802d287c6f05634d38059f531203e8a292d7ef37
Kosteyyy <7456052@gmail.com>
2025-04-05 21:41:27 +0300

    Отслеживание изменений в директивах

--- src/app/cellColorSwitcher.directive.ts
+++ src/app/cellColorSwitcher.directive.ts
@@ -1,4 +1,12 @@
-import { ContentChild, Directive, Input, SimpleChanges } from '@angular/core';
+import {
+    ChangeDetectorRef,
+  ContentChild,
+  ContentChildren,
+  Directive,
+  Input,
+  QueryList,
+  SimpleChanges,
+} from '@angular/core';
 import { PaCellColor } from './cellColor.directive';
 
 @Directive({
@@ -8,12 +16,32 @@ export class PaCellColorSwitcher {
   @Input('paCellDarkColor')
   modelProperty: Boolean | undefined;
 
-  @ContentChild(PaCellColor)
-  contentChild: PaCellColor | undefined;
+  constructor(private cdr: ChangeDetectorRef){}
+
+  //   @ContentChild(PaCellColor)
+  //   contentChild: PaCellColor | undefined;
+
+  @ContentChildren(PaCellColor, { descendants: true })
+  contentChildren: QueryList<PaCellColor> | undefined;
 
   ngOnChanges(changes: SimpleChanges) {
-    if (this.contentChild != null) {
-      this.contentChild.setColor(changes['modelProperty'].currentValue);
+    this.updateContentChildren(changes['modelProperty'].currentValue);
+  }
+
+  ngAfterContentInit() {
+    if (this.modelProperty != undefined) {
+      this.contentChildren?.changes.subscribe(() => {
+        this.updateContentChildren(this.modelProperty as Boolean);
+      });
+    }
+  }
+
+  private updateContentChildren(dark: Boolean) {
+    if (this.contentChildren != null && dark != undefined) {
+      this.contentChildren.forEach((child, index) => {
+        child.setColor(index % 2 ? dark : !dark);
+      });
+      this.cdr.detectChanges(); // Запускаем проверку изменений
     }
   }
 }
